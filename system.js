183bca5c5a2b7876df6c9bdab9c52320ba5ff48d
PraveenG220 <praveen.ganganlli220@gmail.com>
2022-11-03 18:37:59 +0530

    first commit

---
+++ system.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+rightnav = document.querySelector('.rightnav')
+
+
+burger .addEventListener('click', ()=>{
+     navbar.classList.toggle('v-class-resp');
+     navList.classList.toggle('v-class-resp');
+     navbar.classList.toggle('h-nav-resp');
+})
